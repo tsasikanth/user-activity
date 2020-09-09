29c1a381f2e358ebf64270401e1a511d20004373
sasikanth <sasikanth3352@gmail.com>
2020-09-09 14:30:55 +0530

    Updates

---
+++ precache-manifest.fa7b3cab854fab51fd812ffe760ff266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46547804e0f0f1fcebe01dba276bab7",
+    "url": "/user-activity/index.html"
+  },
+  {
+    "revision": "a93b91c119336935af08",
+    "url": "/user-activity/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "fd8823c4ff6d34dc2dd3",
+    "url": "/user-activity/static/css/main.ebf84715.chunk.css"
+  },
+  {
+    "revision": "a93b91c119336935af08",
+    "url": "/user-activity/static/js/2.0c060ed4.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/user-activity/static/js/2.0c060ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8823c4ff6d34dc2dd3",
+    "url": "/user-activity/static/js/main.1e068b57.chunk.js"
+  },
+  {
+    "revision": "4e67137f2dbbe2fb730c",
+    "url": "/user-activity/static/js/runtime-main.2e0cbf9c.js"
+  }
+]);
